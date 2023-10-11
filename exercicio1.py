8c172fd8f61f24a0af5a3160e1238ef1d267a77d
viniciussilva512 <marvosvinicius128@gmail.com>
2023-10-10 21:56:46 -0300

    atividade media

---
+++ exercicio1.py
@@ -0,0 +1,5 @@
+nota01 = float(input("digite a nota 01"))
+nota02 = float(input("digite a nota 02"))
+nota03 = float(input("digite a nota 03"))
+media = (nota01 + nota02 + nota03)/3
+print("sua media é:" , media)
